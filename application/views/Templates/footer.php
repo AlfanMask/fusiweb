7afb4f8409063752366f57bacb665d4de059e167
alfanmask <alfanh123123@gmail.com>
2021-03-04 15:14:36 +0700

    navbar and jumbotron home page

--- application/views/Templates/footer.php
+++
@@ -1,7 +0,0 @@
-
-	<script type="text/javascript"><?= base_url('assets/js/bootstrap/bootstrap.js') ?></script>
-	<script type="text/javascript"><?= base_url('assets/js/all.js') ?></script>
-	<script type="text/javascript"><?= base_url('assets/js/jquery-3.3.1.min.js') ?></script>
-
-</body>
-</html>
--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,18 @@
-HELLO WORLD!
+<!-- Jumbotron -->
+<div class="jumbotron">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="jumbotron-text col-md-4">
+        <h1>Welcome!</h1>
+        <p>di website resmi FUSI (Forum Ukhuwah dan Studi Islam). FUSI merupakan Lembaga Dakwah Kampus Fakultas Pertanian UNS.</p>
+        <button class="btn btn-primary">Read More</button>
+      </div>
+      <div class="jumbotron-img col-md-8">
+        <img src="<?= base_url('assets/img/img-jumbotron.png') ?>">
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END OF Jumbotron -->
 
-<i class="fas fa-chess-board"></i>
+<!-- <div class="container-fluid" style="height:100px; width:100%;">nadanada</div> -->
